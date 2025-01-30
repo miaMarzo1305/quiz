537d1731b861d4253de010ad6ecb6ab7c225b52e
miaMarzo1305 <mia_marzo@biswashington.org>
2025-01-30 17:37:13 +0000

    some message here

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-
+score = 0
+ 
+question = "what is the capital of ecuador?"
+print(question)
+user_answer = input("your answer: ")
+score += 1
+if user_answer.lower() == "quito":
+    print("Correct!")
+ 
+else:
+    print("oops! the correct answer is quito")
+print(f"your score is: {score}")
+ 
+score = 0
+ 
+question = "what is the capital of the USA?"
+print(question)
+user_answer = input("your answer: ")
+score += 1
+if user_answer.lower() == "dc":
+    print("Correct!")
+ 
+else:
+    print("oops! the correct answer is dc")
+print(f"your score is: {score}")
